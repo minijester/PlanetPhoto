4fdf8a91948c5f3e1c34946a968f4ff79ffdbce4
Warut Sathorn <rt.digital1234@gmail.com>
2022-05-29 18:03:57 +0700

    Integrate item apod viewholder

--- app/src/main/java/com/miharu/planetphoto/presenter/feature/apodlist/ApodListAdapter.kt
+++ app/src/main/java/com/miharu/planetphoto/presenter/feature/apodlist/ApodListAdapter.kt
@@ -5,6 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.miharu.planetphoto.R
 import com.miharu.planetphoto.databinding.ItemApodListBinding
 import com.miharu.planetphoto.domain.model.ApodResponse
 
@@ -28,7 +29,18 @@ class ApodListAdapter(
         RecyclerView.ViewHolder(binding.root) {
 
         fun bind(apod: ApodResponse) {
-
+            binding.apply {
+                Glide.with(root)
+                    .load(apod.url)
+                    .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
+                    .into(apodImageView)
+                apodTitleTextView.text = root.context.getString(R.string.apod_title, apod.title)
+                apodDateTextView.text = root.context.getString(R.string.apod_date, apod.date)
+                apodCopyRightTextView.text =
+                    root.context.getString(R.string.apod_copyright, apod.copyright)
+                apodExplanationTextView.text =
+                    root.context.getString(R.string.apod_explanation, apod.explanation)
+            }
         }
     }
 }
